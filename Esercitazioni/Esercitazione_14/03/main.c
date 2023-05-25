bf7610d6fe2e1087c333de166dd8272d510c2fcd
nicoaccardo <nicoloacc@gmail.com>
2023-05-25 11:21:52 +0200

    sistemazione

---
+++ Esercitazioni/Esercitazione_14/03/main.c
@@ -0,0 +1,16 @@
+//* Programma per la lettura da file dei campi di una struttura
+#include <stdio.h>
+
+typedef struct
+{
+    char nome[15];
+    char cognome[20];
+    int voto;
+} alunno;
+
+int main(int argc, char *argv[]){
+
+    
+
+    return 0;
+}
